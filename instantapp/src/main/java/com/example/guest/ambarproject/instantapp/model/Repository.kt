54fba8a09be0d77d7fd89a67f1f8986660c646b7
MatheusMesquita <matheus.mesquita.santos@usp.br>
2019-01-06 01:18:27 -0200

    Added Repository model

---
+++ instantapp/src/main/java/com/example/guest/ambarproject/instantapp/model/Repository.kt
@@ -0,0 +1,6 @@
+package com.example.guest.ambarproject.instantapp.model
+
+data class Repository(val id: Int, val name: String, val full_name: String, val owner: Owner, val description: String, val stargazers_url: String)
+data class Owner(val login: String, val avatar_url: String, val html_url: String)
+
+//https://api.github.com/search/repositories?q=user%3Astrongloop+repo%3Aexpress+express FORKS QUERY
