1dfc09b235336baa2a9b573e5c3004ba16a88cae
MatheusMesquita <matheus.mesquita.santos@usp.br>
2019-01-09 18:38:09 -0200

    Optimized imports

--- instantapp/src/main/java/com/example/guest/ambarproject/instantapp/activity/MainActivity.kt
+++ instantapp/src/main/java/com/example/guest/ambarproject/instantapp/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.guest.ambarproject.instantapp.activity
 
 import android.os.Bundle
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
