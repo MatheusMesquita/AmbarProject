d547a9e6d9059756cf9edabdd58673004af22b55
MatheusMesquita <matheus.mesquita.santos@usp.br>
2019-01-09 18:37:42 -0200

    Added Swipe Refresh

--- instantapp/src/main/java/com/example/guest/ambarproject/instantapp/activity/MainActivity.kt
+++ instantapp/src/main/java/com/example/guest/ambarproject/instantapp/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.guest.ambarproject.instantapp.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import com.example.guest.ambarproject.instantapp.R
 import com.example.guest.ambarproject.instantapp.adapter.RepositoryListAdapter
 import com.example.guest.ambarproject.instantapp.model.Repository
@@ -23,25 +25,35 @@ class MainActivity : AppCompatActivity() {
 
         mRepositories = emptyList()
 
-        val call = RetrofitInitializer().repoService().repositories()
-        call.enqueue(object: Callback<List<Repository>> {
-            override fun onResponse(call: Call<List<Repository>?>?,
-                                    response: Response<List<Repository>?>?) {
-
-                response?.body()?.let {
-                    mRepositories = it.subList(0, 99)
-                    mAdapter.repositories = mRepositories
-                }
-            }
-
-            override fun onFailure(call: Call<List<Repository>?>?,
-                                   t: Throwable?) {
-            }
-        })
+        callRepos()
 
         mAdapter = RepositoryListAdapter(this@MainActivity)
         mAdapter.repositories = mRepositories
         rvRepoList.adapter = mAdapter
         rvRepoList.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        swipeRefresh.setOnRefreshListener {
+            callRepos()
+        }
+    }
+
+    private fun callRepos() {
+        val call = RetrofitInitializer().repoService().repositories()
+        call.enqueue(object: Callback<List<Repository>> {
+            override fun onResponse(call: Call<List<Repository>?>?, response: Response<List<Repository>?>?) {
+                response?.body()?.let {
+                    mRepositories = it.subList(0, 99)
+                    mAdapter.repositories = mRepositories
+                    if (swipeRefresh.isRefreshing)
+                        swipeRefresh.isRefreshing = false
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repository>?>?, t: Throwable?) {
+                Toast.makeText(this@MainActivity, "Problem receiving data", Toast.LENGTH_LONG).show()
+                if (swipeRefresh.isRefreshing)
+                    swipeRefresh.isRefreshing = false
+            }
+        })
     }
 }
